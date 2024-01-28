64c4f23f16e7a32ad4e94791b4cba9bb35f05c36
Udo Schöfer <udos86@users.noreply.github.com>
2024-01-28 19:09:25 +0100

    updates types.ts

--- dynamic/types.ts
+++ dynamic/types.ts
@@ -1,11 +1,20 @@
+/*
 type DotPathOf<Type extends Record<KeyType, unknown>, KeyType extends string | number = string> = {
   [Key in Extract<keyof Type, KeyType>]: Type[Key] extends Array<any>
   ? Key
   : Type[Key] extends Record<KeyType, unknown>
-  ? `${Key}` | `${Key}.${KeyOf<Type[Key], KeyType>}`
+  ? `${Key}` | `${Key}.${DotPathOf<Type[Key], KeyType>}`
   : Key
 }[Extract<keyof Type, KeyType>];
+*/
 
+type DotPathOf<T extends Record<string, unknown>, Key = keyof T> = Key extends string
+  ? T[Key] extends Record<string, unknown>
+    ? `${Key}.${DotPathOf<T[Key]>}`
+    : `${Key}`
+  : never;
+
+/*
 export type PathOf<Type extends Record<string, unknown>, _Acc extends string[] = []> = {
   [Key in Extract<keyof Type, string>]: Type[Key] extends Array<unknown>
   ? [..._Acc, Key]
@@ -13,6 +22,12 @@ export type PathOf<Type extends Record<string, unknown>, _Acc extends string[] =
     ? PathOf<Type[Key], [..._Acc, Key]>
     : [..._Acc, Key];
 }[Extract<keyof Type, string>];
+*/
+export type PathOf<Type extends Record<string, unknown>, _Acc extends string[] = [], Key = keyof Type> = Key extends string
+  ? Type[Key] extends Record<string, unknown>
+    ? PathOf<Type[Key], [..._Acc, Key]>
+    : [..._Acc, Key]
+  : never;
 
 export type TypeOfPath<Type extends Record<string, unknown>, Path extends string[], _Acc extends number[] = []> =
   Type[Path[_Acc['length']]] extends Record<string, unknown>
